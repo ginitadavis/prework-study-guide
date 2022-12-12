e5fce8e147962edba68d15b71a9f8bb2868d6eef
ginitadavis <120149064+ginitadavis@users.noreply.github.com>
2022-12-12 11:45:18 -0500

    I am adding functions created with Javascript to my prework guide

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,27 @@
-/*
-if(topic === 'HTML'){
-    console.log("Let's study HTML");
-} else if(topic === 'CSS'){
-    console.log("Let's study CSS");
-} else if (topic === 'GIT'){
-    console.log("Let's study GIT");
-} else if (topic==='Javascript'){
-    console.log("Let's study Javascript");
-} else {
-    console.log('Please try again!');
-}
-*/
-
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics [Math.floor(Math.random() * topics.length)];
 
-for (var x = 0 ; x < topics.length ; x++){
-    console.log(topics[x]);
+function listTopics(){
+    for (var x = 0 ; x < topics.length ; x++){
+        console.log(topics[x]);
+    }
 }
+
+function selectTopic(){
+    if(randomTopic === 'HTML'){
+        console.log("Let's study HTML");
+    } else if(randomTopic === 'CSS'){
+        console.log("Let's study CSS");
+    } else if (randomTopic === 'GIT'){
+        console.log("Let's study GIT");
+    } else if (randomTopic ==='Javascript'){
+        console.log("Let's study Javascript");
+    } else {
+        console.log('Please try again!');
+    }
+}
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
